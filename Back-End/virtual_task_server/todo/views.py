8c6689418ef43f4ae509c9d6548a829d116abf7b
Ashu <ashutoshkhade2530@gmail.com>
2025-03-24 10:49:55 +0530

    Added all backed required files and dependencies

---
+++ Back-End/virtual_task_server/todo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+def welcome_page(request):
+  return render(request, 'taskapp/welcome.html')
+
+class TodoView(viewsets.ModelViewSet):
+  serializer_class =TodoSerializer
+  queryset =Todo.objects.all()
