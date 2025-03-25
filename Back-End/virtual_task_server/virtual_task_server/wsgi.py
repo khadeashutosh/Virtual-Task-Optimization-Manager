4252cc388d6dd76f9276cc901986aa0c1ab295a7
Ashu <ashutoshkhade2530@gmail.com>
2025-03-25 14:43:00 +0530

    Added WSGI configuration for production deployement

--- Back-End/virtual_task_server/virtual_task_server/wsgi.py
+++ Back-End/virtual_task_server/virtual_task_server/wsgi.py
@@ -2,7 +2,6 @@
 WSGI config for virtual_task_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
